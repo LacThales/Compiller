7c0fc51a068273b10303f3f4d90a83eedbf3d3af
João Pedro <69048604+akajhon@users.noreply.github.com>
2022-11-21 08:11:56 -0300

    Updated run.sh script

--- antlr/run.sh
+++ antlr/run.sh
@@ -1,3 +1,44 @@
-java -jar /usr/local/lib/antlr-4.10.1-complete.jar atribuicao.g4
-javac *.java
-java Main < entrada.txt
+#!/bin/sh
+
+Banner()
+{
+banner = '''
+
+		 █████                ███████████            █████      
+		░░███                ░░███░░░░░███          ░░███       
+		 ░███                 ░███    ░███           ░███       
+		 ░███                 ░██████████            ░███       
+		 ░███                 ░███░░░░░███           ░███       
+		 ░███      █          ░███    ░███           ░███      █
+		 ███████████    ██    █████   █████    ██    ███████████
+		░░░░░░░░░░░    ░░    ░░░░░   ░░░░░    ░░    ░░░░░░░░░░░ 
+                                                         
+                                         The Lazy Recursive Language
+                                                        Version: 1.0
+                                                        
+            [!] A simple language.... for the lazy ones [!]
+''' 
+
+echo banner
+}
+
+Main()
+{
+	java -jar /usr/local/lib/antlr-4.10.1-complete.jar atribuicao.g4
+	javac *.java
+	java Main < entrada.txt
+}
+
+extra()
+{	
+	
+	xdg-open output_img.ps
+	javac out.java
+	java out
+}
+
+Banner
+echo "[!] Starting the Compiling process... [!]"
+Main
+extra
+echo "[!] End of Compiling... Sucess [!]"
